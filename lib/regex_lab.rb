072f07de6156b823cec653e2df55c79c7bee9b32
Micah McCallum <micahmccallum@outlook.com>
2020-03-25 02:18:44 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,8 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  /([A-Z])(.*)([.?!])/.match(text)[0]
-  
+  /([A-Z])(.*)([.?!])/.match(text)[0] == text
 end
 
 def valid_phone_number?(phone)
